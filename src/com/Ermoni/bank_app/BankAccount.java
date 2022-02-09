f92eabbd5f17266e6353a530b0ebd0b67bf2a2fe
Ermoni Osborne <moni.osborne@yahoo.com>
2022-02-08 21:14:54 -0500

    Add methods for checking acct

--- src/com/Ermoni/bank_app/BankAccount.java
+++ src/com/Ermoni/bank_app/BankAccount.java
@@ -2,15 +2,28 @@ package com.Ermoni.bank_app;
 
 public class BankAccount {
     private double checkingBalance = 0.00;
-    private double savingBalance = 0.00;
+    private double savingsBalance = 0.00;
     final private double checkingOpeningDeposit;
 
     private final double savingsOpeningDeposit;
-
+//Created constructors
     public BankAccount(double checkingOpeningDeposit, double savingsOpeningDeposit) {
         this.checkingOpeningDeposit = checkingOpeningDeposit;
         this.savingsOpeningDeposit = savingsOpeningDeposit;
         checkingBalance += this.checkingOpeningDeposit;
-        savingsOpeningDeposit += this.savingsOpeningDeposit;
+        savingsBalance += this.savingsOpeningDeposit;
     }
+    //Create methods to
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+     public double getSavingsBalance() {
+        return savingsBalance;
+     }
+
+    
+
+
+
 }
--- src/com/Ermoni/bank_app/BankApp.java
+++ src/com/Ermoni/bank_app/BankApp.java
@@ -2,7 +2,9 @@ package com.Ermoni.bank_app;
 
 public class BankApp {
     public static void main(String[] args) {
-
+        BankAccount customerAcct = new BankAccount(2000,1000);
+        System.out.println(customerAcct.getCheckingBalance());
+        System.out.println(customerAcct.getSavingsBalance());
     }
 
 }
