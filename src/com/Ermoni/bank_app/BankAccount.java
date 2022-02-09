65d7a96328c0f2e77cb85dfa9ea86fa334c645cc
Ermoni Osborne <moni.osborne@yahoo.com>
2022-02-08 21:32:38 -0500

    Create instance to prevent account from overdrafting

--- src/com/Ermoni/bank_app/BankAccount.java
+++ src/com/Ermoni/bank_app/BankAccount.java
@@ -21,12 +21,22 @@ public class BankAccount {
      public double getSavingsBalance() {
         return savingsBalance;
      }
-//Create constructors for withdrawals for checking and savings account
+//Create constructors for withdrawals for checking and savings account and added condition to prevent overdrafts
     public void checkingWithdrawal(double withdrawalAmount) {
-        checkingBalance -= withdrawalAmount;
+        if (checkingBalance < withdrawalAmount){
+            System.out.println("Your account is insufficient for this transaction");
+            if(savingsBalance > withdrawalAmount) {
+                System.out.println('You need to do a balance transfer first');
+            }
+        } else if (checkingBalance > withdrawalAmount){
+            checkingBalance -= withdrawalAmount;
+        }
     }
     public void savingsWithdrawal(double savingsWithdrawalAmount) {
         savingsBalance -= savingsWithdrawalAmount;
     }
+
+    //Create ___ to prevent overdraft of account
+
 
 }
